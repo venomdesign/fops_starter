3af8d3c01a9c703bee4f9b87044dbfec5a3a465f
Schweigart <Chris.Schweigart@fnf.com>
2017-10-04 16:02:12 -0400

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { Http, HttpModule, RequestOptions } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { ChartsModule } from 'ng2-charts';
+import { ToastModule } from 'ng2-toastr/ng2-toastr';
+
+import { AppComponent } from './app.component';
+
+import { AuthGuardService } from './services/auth-guard.service';
+import { AuthService } from './services/auth.service';
+import { GlobalService } from './services/global.service';
+import { ScopeGuardService } from './services/scope-guard.service';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { CallbackComponent } from './callback/callback.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        ProfileComponent,
+        HomeComponent,
+        CallbackComponent,
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        HttpModule,
+        AppRoutingModule,
+        SlimLoadingBarModule.forRoot(),
+        ToastModule.forRoot(),
+        [
+            ChartsModule
+        ]
+    ],
+    providers: [
+        AuthService,
+        AuthGuardService,
+        GlobalService,
+        ScopeGuardService
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
